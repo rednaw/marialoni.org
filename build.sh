e7d15dab3f6cea8402af28c5ce3c2bff605ca851
Wander Grevink <wander.grevink@gmail.com>
2025-09-23 16:58:47 +0200

    liquid  warnings

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo "Building Jekyll site..."
 echo ""
-output=$(RUBYOPT="-W0" bundle exec jekyll build 2>&1 | grep -v "Warning: Empty")
+output=$(RUBYOPT="-W0" bundle exec jekyll build --strict 2>&1 | grep -v "Warning: Empty")
 
-if echo "$output" | grep -q "Exception\|Error\|Failed"; then
+if echo "$output" | grep -q "Exception\|Error\|Failed\|Warning"; then
     echo "$output" | grep "YAML Exception"
     echo ""
     echo "$output" | grep "Failed to parse BibTeX" | head -1
+    echo ""
+    echo "$output" | grep "Liquid Warning" | head -1
     echo ""
     echo "💡 Fix the errors above and try again"
     echo ""
